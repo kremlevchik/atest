61ebc452c34a627f868c0282c3391b5b5a109f81
Ivan <kremlevchik98@mail.ru>
2024-02-21 22:16:47 +0300

    2

--- Program.cs
+++ Program.cs
@@ -5,29 +5,72 @@
 // использовать циклы.
 
 
-int PrintNumbers(int x, int y)
-{
-    if (x > y)
-    {
-        Console.Write($"{y}, ");
-        if (x == y)
-        {
-            return (y);
-        }
+// int PrintNumbers(int x, int y)
+// {
+//     if (x > y)
+//     {
+//         Console.Write($"{y}, ");
+//         if (x == y)
+//         {
+//             return (y);
+//         }
 
-        return PrintNumbers(x, y + 1);
-    }
-    Console.Write($"{x}, ");
-    if (x == y)
+//         return PrintNumbers(x, y + 1);
+//     }
+//     Console.Write($"{x}, ");
+//     if (x == y)
+//     {
+//         return (x);
+//     }
+//     return PrintNumbers(x + 1, y);
+// }
+// Console.Clear();
+// Console.Write("Задайте значение M: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("Задайте значение N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// PrintNumbers(m, n);
+
+// Задача 2: 
+// Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
+int Ackermann(int m, int n)
+{
+    if (m < 0 || n < 0)
     {
-        return (x);
+        return (-1);
     }
-    return PrintNumbers(x + 1, y);
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    return (0);
 }
+void PrintResult(int k)
+{
+    if (k == -1)
+    {
+        Console.WriteLine("Значение n и m не могут быть отрицательными числами!");
+    }
+    else
+        Console.WriteLine(k);
+}
+
 Console.Clear();
-Console.Write("Задайте значение M: ");
+Console.Write("Задайте значение m: ");
 int m = int.Parse(Console.ReadLine()!);
-Console.Write("Задайте значение N: ");
+Console.Write("Задайте значение n: ");
 int n = int.Parse(Console.ReadLine()!);
-PrintNumbers(m, n);
+int k = Ackermann(m, n);
+PrintResult(k);
 

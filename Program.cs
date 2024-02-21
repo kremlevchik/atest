cbaaf7c3d07a06c0435ef60fb10de5dc3dda02b3
Ivan <kremlevchik98@mail.ru>
2024-02-21 22:17:07 +0300

    3

--- Program.cs
+++ Program.cs
@@ -36,41 +36,83 @@
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
 
-int Ackermann(int m, int n)
+// int Ackermann(int m, int n)
+// {
+//     if (m < 0 || n < 0)
+//     {
+//         return (-1);
+//     }
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     if (m > 0 && n == 0)
+//     {
+//         return Ackermann(m - 1, 1);
+//     }
+//     if (m > 0 && n > 0)
+//     {
+//         return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+//     return (0);
+// }
+// void PrintResult(int k)
+// {
+//     if (k == -1)
+//     {
+//         Console.WriteLine("Значение n и m не могут быть отрицательными числами!");
+//     }
+//     else
+//         Console.WriteLine(k);
+// }
+
+// Console.Clear();
+// Console.Write("Задайте значение m: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("Задайте значение n: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = Ackermann(m, n);
+// PrintResult(k);
+// Задача 3: 
+// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+int[] FillingArray(int[] array, int i)
 {
-    if (m < 0 || n < 0)
+    if (i < array.Length)
     {
-        return (-1);
+        array[i] = new Random().Next(1, 100);
+        FillingArray(array, i + 1);
     }
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    if (m > 0 && n == 0)
-    {
-        return Ackermann(m - 1, 1);
-    }
-    if (m > 0 && n > 0)
-    {
-        return Ackermann(m - 1, Ackermann(m, n - 1));
-    }
-    return (0);
+    return array;
 }
-void PrintResult(int k)
+
+string PrintStartArray(int[] array, int i)
 {
-    if (k == -1)
+    if (i == array.Length - 1)
     {
-        Console.WriteLine("Значение n и m не могут быть отрицательными числами!");
+        return $"{array[array.Length - 1]} ";
     }
-    else
-        Console.WriteLine(k);
+    return PrintStartArray(array, i + 1) + $"{array[i]} ";
+}
+
+string PrintEndArray(int[] array, int i)
+{
+    if (i == array.Length - 1)
+    {
+        return $"{array[array.Length - 1]} ";
+    }
+    return $"{array[i]} " + PrintEndArray(array, i + 1);
 }
 
 Console.Clear();
-Console.Write("Задайте значение m: ");
-int m = int.Parse(Console.ReadLine()!);
-Console.Write("Задайте значение n: ");
+Console.Write("Задайте количество элементов в массиве: ");
 int n = int.Parse(Console.ReadLine()!);
-int k = Ackermann(m, n);
-PrintResult(k);
-
+int i = 0;
+int[] array = new int[n];
+FillingArray(array, i);
+Console.WriteLine("Начальный массив: ");
+Console.WriteLine(PrintStartArray(array, i));
+Console.WriteLine("Конечный массив: ");
+Console.WriteLine(PrintEndArray(array, i));
